036a1a27b60593065a56740f1970d29066e5e47a
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-09-13 14:07:35 +0300

    Debug welcome email credentials

--- django_gram/settings.py
+++ django_gram/settings.py
@@ -134,12 +134,12 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# Email configurations
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'test.user.python3.6@gmail.com'
+EMAIL_HOST_PASSWORD = 'codingstudent001'
 
 LOGIN_REDIRECT_URL='timeline'
 LOGOUT_REDIRECT_URL='home'
